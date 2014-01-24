f436e79a667480868829ac6dac1f8460682ca847
Katerina <katekson@gmail.com>
2014-01-24 15:34:04 -0800

    Initializing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WatchMeRun::Application.config.secret_key_base = '712e7532825fa7a4a86cd28f5707eaf6ef1107d8e233927c20ad17b3ed96e87fc3c05168bfeb3000acdd05589ab1b0da155672cd0e3bd5cab6eb3aa6708bb812'
